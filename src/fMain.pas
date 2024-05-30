358e36dc7ab5ae7a8b4e8aa01f6d4bfd6d3f7f63
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-30 14:58:16 +0200

    updated description and license information in the program about box dialog (#35)

--- src/fMain.pas
+++ src/fMain.pas
@@ -50,6 +50,7 @@ type
     procedure SetCurrentScreen(const Value: TRootFrame);
   protected
     procedure InitMainFormCaption;
+    procedure InitAboutDialogDescriptionAndLicense;
   public
     { Déclarations publiques }
     property CurrentScreen: TRootFrame read FCurrentScreen
@@ -74,6 +75,8 @@ procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   InitMainFormCaption;
 
+  InitAboutDialogDescriptionAndLicense;
+
 {$IF Defined(MACOS)}
   mnuQuit.Visible := false; // added by default in Mac system menu
   mnuQuit.text := mnuQuit.text + ' ' + OlfAboutDialog1.Titre;
@@ -86,6 +89,37 @@ begin
   mnuAbout.text := mnuAbout.text + ' ' + OlfAboutDialog1.Titre;
 
   FCurrentScreen := nil;
+end;
+
+procedure TfrmMain.InitAboutDialogDescriptionAndLicense;
+begin
+  OlfAboutDialog1.Licence.Text :=
+    'This program is distributed as shareware. If you use it (especially for ' +
+    'commercial or income-generating purposes), please remember the author and '
+    + 'contribute to its development by purchasing a license.' + slinebreak +
+    slinebreak +
+    'This software is supplied as is, with or without bugs. No warranty is offered '
+    + 'as to its operation or the data processed. Make backups!';
+  OlfAboutDialog1.Description.Text :=
+    'Utility for managing a local database of series and episodes published or to be published on video-on-demand sites (such as YouTube).'
+    + slinebreak + slinebreak + '*****************' + slinebreak +
+    '* Publisher info' + slinebreak + slinebreak +
+    'This application was developed by Patrick Prémartin.' + slinebreak +
+    slinebreak +
+    'It is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.'
+    + slinebreak + slinebreak + '****************' + slinebreak +
+    '* Personal data' + slinebreak + slinebreak +
+    'This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.'
+    + slinebreak + slinebreak + 'We have no knowledge of what you do with it.' +
+    slinebreak + slinebreak +
+    'No information about you is transmitted to us or to any third party.' +
+    slinebreak + slinebreak +
+    'We use no cookies, no tracking, no stats on your use of the application.' +
+    slinebreak + slinebreak + '**********************' + slinebreak +
+    '* User support' + slinebreak + slinebreak +
+    'If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.'
+    + slinebreak + slinebreak +
+    'To find out more, visit https://ytvideoseries.olfsoftware.fr';
 end;
 
 procedure TfrmMain.InitMainFormCaption;
