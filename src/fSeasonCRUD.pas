23556208a1cfd163e071122ea70f55870a18b6a9
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-31 16:33:47 +0200

    added a record state check and save/cancel confirmation when we hide/close the "form" (#27)

--- src/fSeasonCRUD.pas
+++ src/fSeasonCRUD.pas
@@ -126,6 +126,7 @@ implementation
 {$R *.fmx}
 
 uses
+  FMX.DialogService,
   System.DateUtils,
   u_urlOpen,
   fSelectRecord,
@@ -280,8 +281,24 @@ end;
 
 procedure TfrmSeasonCRUD.OnHide;
 begin
-  // TODO : tester si record en ajout/modif pour demander confirmation avant
-  FDTable1.Active := false;
+  if FDTable1.State in dsEditModes then
+  begin
+    TDialogService.MessageDialog
+      ('This record has been edited. Do you want to save the changes ?',
+      TMsgDlgType.mtWarning, mbyesno, tmsgdlgbtn.mbYes, 0,
+      procedure(const AResult: TModalResult)
+      begin
+        case AResult of
+          mryes:
+            FDTable1.Post;
+        else
+          FDTable1.Cancel;
+        end;
+        FDTable1.Active := false;
+      end);
+  end
+  else
+    FDTable1.Active := false;
   inherited;
 end;
 
