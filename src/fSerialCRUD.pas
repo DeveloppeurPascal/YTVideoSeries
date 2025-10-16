b85155a9df6df80ba2998326f8c947bcface1e52
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-16 19:28:20 +0200

    added copyright and timestamp comments

--- src/fSerialCRUD.pas
+++ src/fSerialCRUD.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  YT Video Series
+
+  Copyright 2023-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://ytvideoseries.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/YTVideoSeries
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:21.466+02:00
+  Signature : 6356528e5a306e89af2b482c01ab5728c7656652
+  ***************************************************************************
+*)
+
 unit fSerialCRUD;
 
 interface
--- src/fVideoTubeLinkCRUD.pas
+++ src/fVideoTubeLinkCRUD.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  YT Video Series
+
+  Copyright 2023-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://ytvideoseries.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/YTVideoSeries
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:21.466+02:00
+  Signature : 8401ad92ce5064d14c415ae36b2bcfb9b4e39bfe
+  ***************************************************************************
+*)
+
 unit fVideoTubeLinkCRUD;
 
 interface
